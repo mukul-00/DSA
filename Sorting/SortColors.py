f2589c6bd58e5a25ea85e0333139082a578b3783
Mukul Rajput <mukulrajput0707@gmail.com>
2026-03-03 18:11:05 +0530

    sort an array of 0's, 1's and 2's

---
+++ Sorting/SortColors.py
@@ -0,0 +1,52 @@
+#-------------BETTER SOLUTION-----------------------------
+
+# nums = [1, 2, 0, 2, 1, 0, 0, 1]
+
+# cnt0 = 0
+# cnt1 = 0
+# cnt2 = 0
+
+# # Count 0s, 1s, 2s
+# for i in range(len(nums)):
+#     if nums[i] == 0:
+#         cnt0 += 1
+#     elif nums[i] == 1:
+#         cnt1 += 1
+#     else:
+#         cnt2 += 1
+
+# # Fill 0s
+# for i in range(cnt0):
+#     nums[i] = 0
+
+# # Fill 1s
+# for i in range(cnt0, cnt0 + cnt1):
+#     nums[i] = 1
+
+# # Fill 2s
+# for i in range(cnt0 + cnt1, cnt0 + cnt1 + cnt2):
+#     nums[i] = 2
+
+# print(nums)
+
+    
+#-----------------OPTIMAL SOLUTION-------------------------
+nums = [1, 2, 0, 2, 1, 0, 0, 1]
+
+low = 0
+mid = 0
+high = len(nums) - 1
+
+while (mid <= high):
+    if(nums[mid] == 0):
+        nums[low], nums[mid] = nums[mid], nums[low]
+        low += 1
+        mid += 1
+
+    elif(nums[mid] == 1):
+        mid += 1
+
+    else:
+        nums[mid], nums[high] = nums[high], nums[mid]
+        high -= 1
+print(nums)
